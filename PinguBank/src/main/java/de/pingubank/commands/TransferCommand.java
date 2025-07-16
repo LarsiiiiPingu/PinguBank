c45181fd458bfaea5edba8703a809fd220229f85
𝕷𝖆𝖗𝖘𝖎𝖎𝖎𝖎 <123985584+LarsiiiiPingu@users.noreply.github.com>
2025-07-16 11:14:51 +0200

    Create TransferCommand.java

---
+++ PinguBank/src/main/java/de/pingubank/commands/TransferCommand.java
@@ -0,0 +1,59 @@
+package de.pingubank.commands;
+
+import de.pingubank.util.BankManager;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TransferCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player senderPlayer)) {
+            sender.sendMessage("§cNur Spieler können diesen Befehl verwenden.");
+            return true;
+        }
+
+        if (args.length != 2) {
+            senderPlayer.sendMessage("§cVerwendung: /transfer <Spieler> <Betrag>");
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target == null || target == senderPlayer) {
+            senderPlayer.sendMessage("§cUngültiger Spieler.");
+            return true;
+        }
+
+        double betrag;
+        try {
+            betrag = Double.parseDouble(args[1]);
+        } catch (NumberFormatException e) {
+            senderPlayer.sendMessage("§cUngültiger Betrag.");
+            return true;
+        }
+
+        if (betrag <= 0) {
+            senderPlayer.sendMessage("§cDer Betrag muss größer als 0 sein.");
+            return true;
+        }
+
+        double senderKonto = BankManager.getBankBalance(senderPlayer.getUniqueId());
+        if (senderKonto < betrag) {
+            senderPlayer.sendMessage("§cDu hast nicht genug Geld auf deinem Bankkonto.");
+            return true;
+        }
+
+        // Übertragen
+        BankManager.withdraw(senderPlayer, betrag);
+        BankManager.deposit(target, betrag);
+
+        senderPlayer.sendMessage("§7Du Hast Erfolgreich §e" + betrag + " §7An §3" + target.getName() + " §aÜberwiesen§7.");
+        target.sendMessage("§7Du Hast §e" + betrag + " §7Von §3" + senderPlayer.getName() + " §aErhalten.");
+
+        Bukkit.getLogger().info("[Bank] " + senderPlayer.getName() + " Transfer Zu " + target.getName() + " Summe: " + betrag);
+        return true;
+    }
+}
